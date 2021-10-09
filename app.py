d22e018d4c443038fed11095e4eae530c9c039dd
Bailey <bailey.gage.davis@gmail.com>
2021-10-09 20:47:38 +0900

    got the jinja to unpack the 2d array correctly i think

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 from boggle import Boggle
+#Imported by me
+from flask import Flask, request, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+#Setup flask and flask debug toolbar
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route("/")
+def home_page():
+    """Shows home page."""
+    new_board = boggle_game.make_board()
+
+    return render_template("home.html", board=new_board)
