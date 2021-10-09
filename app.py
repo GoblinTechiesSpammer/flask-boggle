a1d56a7ad63295109fe9ede8bd00aca2454cc0a7
Bailey <bailey.gage.davis@gmail.com>
2021-10-09 21:21:38 +0900

    added css and js files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 #Setup flask and flask debug toolbar
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = "test123"
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
@@ -13,6 +13,7 @@ boggle_game = Boggle()
 @app.route("/")
 def home_page():
     """Shows home page."""
-    new_board = boggle_game.make_board()
+    board = boggle_game.make_board()
+    session['board'] = board
 
-    return render_template("home.html", board=new_board)
+    return render_template("home.html")
