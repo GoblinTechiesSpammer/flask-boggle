d4483cb122e67776f1b7308ff6128424362a4527
Bailey <bailey.gage.davis@gmail.com>
2021-10-10 22:56:44 +0900

    integration test is messed up

--- test.py
+++ test.py
@@ -3,13 +3,26 @@ from app import app
 from flask import session
 from boggle import Boggle
 
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont_show_debug_toolbar']
 
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
     
-    def setUp(self):
-        self.client = app.test_client()
-        app.config['TESTING'] = True
+    # def setUp(self):
+    #     self.client = app.test_client()    
     
+    def test_home_page(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<form method="GET" id="word-guess">', html)
     
+    def test_guess(self):
+        with app.test_client() as client:
+            res = client.get("/guess", query_string={'word': 'test'})
+
+            self.assertEqual(res.status_code, 200)
