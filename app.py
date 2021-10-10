e596a2059a3fb726009e41764a645e7cb827b74f
Bailey <bailey.gage.davis@gmail.com>
2021-10-10 14:12:47 +0900

    code refactor

--- app.py
+++ app.py
@@ -13,26 +13,31 @@ boggle_game = Boggle()
 
 @app.route("/")
 def home_page():
-    """Shows home page."""
-    board = boggle_game.make_board()
-    session['board'] = board
-    
-    #Clear score
-    session['score'] = 0
-
+    """Shows home page. Initializes the boggle game."""
+    initialize_game()
     return render_template("home.html")
 
 @app.route("/guess")
 def guess():
-    """Recieve GET request from axios"""
+    """Recieve GET request from axios. Returns the validity of the word and the new score."""
     word = request.args["word"]
     #Already check both, even though instructions say to check each(dictionary and board) independenty.
     validity = boggle_game.check_valid_word(session['board'], word)
 
-    #Adding word length to score.
+    #Adding word length to score if word is valid.
     if validity == 'ok':
-        score = session['score']
-        score += len(word)
-        session['score'] = score
+        update_score(word)
 
     return jsonify({'result': validity, 'score': session['score']})
+
+#Initializes and resets the boggle game.
+def initialize_game():
+    board = boggle_game.make_board()
+    session['board'] = board
+    session['score'] = 0
+
+#Updates the game score with the length of the word.
+def update_score(word):
+    score = session['score']
+    score += len(word)
+    session['score'] = score
--- static/app.js
+++ static/app.js
@@ -1,16 +1,47 @@
+// Event handler for word guess form.
 const $wordGuess = $("#word-guess");
+$wordGuess.submit(wordGuessForm);
 
-$wordGuess.submit(async function(event){
+async function wordGuessForm(event){
     event.preventDefault();
 
     const $guess = $("#guess");
     let word = $guess.val();
     let response = await axios.get("/guess", {params: {word: word}});
     console.log(response.data.result);
-    $(".guess-result").empty();
-    $(".guess-result").append(`<p>${response.data.result}</p>`);
 
-    // Score
+    //Update guess feedback on page.
+    updateGuess(response.data.result);
+
+    //Update score on page.
+    updateScore(response.data.score);
+}
+
+function updateGuess(result){
+    $(".guess-result").empty();
+    $(".guess-result").append(`<p>${result}</p>`);
+}
+
+function updateScore(score){
     $(".score").empty();
-    $(".score").append(`<p>${response.data.score}</p>`);
-});
+    $(".score").append(`<p>${score}</p>`);
+}
+
+
+// Timer for stopping game after 60 seconds.
+const $timer = $(".timer");
+var startTime = 0;
+
+// How would I disable more submissions?
+var time = setInterval(tickTock, 1000);
+
+function tickTock(){
+    if (startTime >= 60){
+        $wordGuess.remove();
+        clearInterval(time);
+    }
+    else{
+        startTime += 1;
+        $timer.text(startTime);
+    }
+}
